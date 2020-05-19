778f0618c193cf6cde8d9fa327cdd429e5e3450d
Pipe Echeverri <pipe.echeverri.1@gmail.com>
2020-05-19 14:46:23 -0500

    AJUSTES LOGIN

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -26,46 +26,48 @@ class ApiController extends ActiveController {
             'class' => \yii\filters\Cors::className(),
             'cors' => [
                 'Origin' => ['*'],
-                'Access-Control-Request-Method' => ['GET', 'HEAD', 'OPTIONS'],
+                'Access-Control-Request-Method' => ['GET', 'HEAD', 'OPTIONS', 'POST'],
                 'Access-Control-Request-Headers' => ['*'],
                 'Access-Control-Allow-Credentials' => false,
             ],
         ];
-        $behaviors['authenticator'] = [
-            'class' => \yii\filters\auth\HttpBasicAuth::className(),
-            'auth' => [$this, 'auth']
-        ];
+//        $behaviors['authenticator'] = [
+//            'class' => \yii\filters\auth\HttpBasicAuth::className(),
+//            'auth' => [$this, 'auth']
+//        ];
         return $behaviors;
     }
 
     protected function verbs() {
         return [
             'view' => ['GET', 'HEAD', 'OPTIONS'],
-            'searchapartment' => ['POST', 'OPTIONS']
+            'searchapartment' => ['POST', 'OPTIONS'],
+            'login' => ['POST', 'OPTIONS'],
+            'logout' => ['POST', 'OPTIONS']
         ];
     }
 
     public function auth($username, $password) {
-        $user = \app\models\User::findByUsername($username);
-
-        //Si usuario y contraseña no es correcto
-        if (!$user || $user->password !== md5($password)) {
-            throw new \yii\web\HttpException(401);
-        }
-        //Verifico la autorizacion
-        $request = \Yii::$app->request;
-        $idUnidadResidencial = $request->get('idUnidadResidencial');
-        $authorization = \app\models\Authorizations::find()
-                ->where([
-                    'user_id' => $user->id,
-                    'housing_estate_id' => $idUnidadResidencial
-                ])
-                ->all();
-        if (!$authorization) {
-            throw new \yii\web\HttpException(401);
-        }
-
-        return $user;
+//        $user = \app\models\User::findByUsername($username);
+//
+//        //Si usuario y contraseña no es correcto
+//        if (!$user || $user->password !== md5($password)) {
+//            throw new \yii\web\HttpException(401);
+//        }
+//        //Verifico la autorizacion
+//        /*$request = \Yii::$app->request;
+//        $idUnidadResidencial = $request->get('idUnidadResidencial');
+//        $authorization = \app\models\Authorizations::find()
+//                ->where([
+//                    'user_id' => $user->id,
+//                    'housing_estate_id' => $idUnidadResidencial
+//                ])
+//                ->all();
+//        if (!$authorization) {
+//            throw new \yii\web\HttpException(401);
+//        }*/
+//
+//        return $user;
     }
 
     public function actions() {
@@ -77,15 +79,36 @@ class ApiController extends ActiveController {
     }
 
     public function actionIndex() {
-        return \app\models\HousingEstate::find()
-                        ->joinWith('city')
-                        ->where(['housing_estate.active' => 1])
-                        ->asArray()
-                        ->all();
+        /* return \app\models\HousingEstate::find()
+          ->joinWith('city')
+          ->where(['housing_estate.active' => 1])
+          ->asArray()
+          ->all(); */
+//        return \app\models\HousingEstate::find()
+//                        ->joinWith('city')
+//                        ->where(['housing_estate.active' => 1])
+//                        ->asArray()
+//                        ->all();
+        var_dump(\Yii::$app->request);
     }
 
-    public function actionView($idUnidadResidencial) {
-        $idUnidad = explode(",", $idUnidadResidencial);
+    public function actionView($user) {
+
+        if (empty($user)) {
+            throw new \yii\web\HttpException(404);
+        }
+
+        $model = \app\models\SecurityGuards::find()
+                ->select("housing_estate_security_guard.housing_estate_id")
+                ->leftJoin('housing_estate_security_guard', 'housing_estate_security_guard.security_guard_id = security_guards.id')
+                ->where('MD5(user_id)= "' . $user . '"')
+                ->asArray()
+                ->all();
+        $idUnidadResidencial = [];
+        foreach ($model as $m) {
+            $idUnidadResidencial[] = $m['housing_estate_id']; 
+        }
+        
         $model = \app\models\HousingEstate::find()
                 ->joinWith('apartments')
                 ->joinWith('gates')
@@ -95,13 +118,34 @@ class ApiController extends ActiveController {
                 ->where([
                     'housing_estate.active' => 1
                 ])
-                ->andWhere(['in', 'housing_estate.id', $idUnidad])
+                ->andWhere(['in', 'housing_estate.id', $idUnidadResidencial])
                 ->asArray()
                 ->all();
         if (!$model) {
             throw new \yii\web\HttpException(404);
         }
         return $model;
+    }
+
+    public function actionLogin() {
+        $username = \Yii::$app->request->getBodyParam('username');
+        $password = \Yii::$app->request->getBodyParam('password');
+
+        $user = \app\models\User::findByUsername($username);
+
+        //Si usuario y contraseña no es correcto
+        if (!$user || $user->password !== md5($password)) {
+            throw new \yii\web\HttpException(401);
+        }
+
+        return $user;
+    }
+
+    public function actionLogout() {
+        if (!\Yii::$app->user->logout()) {
+            throw new \yii\web\HttpException(500);
+        }
+        throw new \yii\web\HttpException(200);
     }
 
     public function actionSearchapartment() {
